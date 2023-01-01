866bdf644e3bb0db0aa8ca5e5423c3e9f26b7584
Alek30k <alemank23k@gmail.com>
2023-01-01 11:07:15 -0300

    last commit

--- filter.js
+++ filter.js
@@ -48,3 +48,35 @@ const searchInput = document.querySelector(".search");
 const categoriesContainer = document.querySelector(".cats");
 const priceRange = document.querySelector(".priceRange");
 const priceValue = document.querySelector(".priceValue");
+
+const displayProducts = (filteredProducts) => {
+  productsContainer.innerHTML = filteredProducts
+    .map(
+      (product) =>
+        `
+    <div class="product">
+       <img
+       src=${product.img}
+       alt=""
+       />
+       <span class="name">${product.name}</span>
+       <span class="priceText">$${product.price}</span>
+     </div>
+ `
+    )
+    .join("");
+};
+
+displayProducts(data);
+
+searchInput.addEventListener("keyup", (e) => {
+  const value = e.target.value.toLowerCase();
+
+  if (value) {
+    displayProducts(
+      data.filter((item) => item.name.toLowerCase().indexOf(value) !== -1)
+    );
+  } else {
+    displayProducts(data);
+  }
+});
